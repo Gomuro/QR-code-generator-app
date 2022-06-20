658d05dbf0bf02c33e9e4fcd3052434529dae43e
Gomuro <yra3908@gmail.com>
2022-06-20 17:30:26 +0300

    done app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import QRCode from "qrcode";
 
 function App() {
+  const [URL, setURL] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+
+  const generateQRCode = () => {
+    QRCode.toDataURL(URL, (err, url) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      setImageUrl(url);
+    });
+  };
+  const downloadQRCode = () => {
+    const link = document.createElement("a");
+    link.href = imageUrl;
+    link.download = "qrcode.png";
+    link.click();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Card>
+        <h2>QR Code Generator</h2>
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={4} md={6} sm={8}>
+              <TextField
+                label="URL"
+                value={URL}
+                onChange={(e) => setURL(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} lg={4} md={6} sm={8}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={generateQRCode}
+              >
+                Generate QR Code
+              </Button>
+            </Grid>
+            {imageUrl && (
+              <Grid item xs={12} lg={4} md={6} sm={8}>
+                <img src={imageUrl} alt="qr-code" />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={downloadQRCode}
+                >
+                  Download QR Code
+                </Button>
+              </Grid>
+            )}
+          </Grid>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
 
